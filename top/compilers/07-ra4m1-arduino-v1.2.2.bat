c119197ea34558bf2c5b6f025df4f68048667b6f
John Taylor <john.t.taylor@gmail.com>
2024-11-10 17:48:21 -0500

    working on toolchain for UNO R4 WIFI

---
+++ top/compilers/07-ra4m1-arduino-v1.2.2.bat
@@ -0,0 +1,16 @@
+echo off
+IF "/%1"=="/name" ECHO:GCC-ARM compiler for Grand Central BSP 1.6.0 & exit /b 0
+
+:: Core Arduino tools
+set ARDUINO_BSP_VER=1.2.2
+set ARDUINO_TOOLS_ROOT=%NQBP_PKG_ROOT%arduino\tools
+
+:: Segger support
+set PATH=%PATH%;c:\Program Files (x86)\SEGGER\JLink
+set PATH=%PATH%;c:\Program Files\SEGGER\Ozone
+
+:: Add Compiler to the path. Note: Leverage the same cross compiler used for the STM32 MCUs
+call set PATH=%NQBP_XPKGS_ROOT%\stm32-gcc-arm\bin;%PATH%
+
+::
+echo:Environment set for ARM-Cortex M4, Uno R4 Wifi (v%ARDUINO_BSP_VER%)
